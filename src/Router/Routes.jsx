8ecaa36ccdd6ee46d9b2d05a268fa1fd9643b9d5
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2024-09-23 14:43:49 +0600

    navbar-basic-complete

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -2,6 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../Root";
 import ErrorElement from "../ErrorElement";
 import Home from "../Pages/Home/Home";
+import LogIn from "../Pages/LogIn/LogIn";
+import Regster from "../Pages/Register/Regster";
+import PropertyDetails from "../Pages/PropertyDetails/PropertyDetails";
+import Connect from "../Pages/Connect/Connect";
 
 const router = createBrowserRouter([
     {
@@ -14,7 +18,20 @@ const router = createBrowserRouter([
                 element:<Home></Home>
             },
             {
-                
+                path:'/logIn',
+                element:<LogIn></LogIn>
+            },
+            {
+                path:'/register',
+                element:<Regster></Regster>
+            },
+            {
+                path:'/details',
+                element:<PropertyDetails></PropertyDetails>
+            },
+            {
+                path:'/connect',
+                element:<Connect></Connect>
             }
         ]
     }
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,64 @@
+import { NavLink } from "react-router-dom";
+import "./navbar.css";
 
 const Navbar = () => {
-    return (
-        <div>
-            
+  const nav = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/details">Details</NavLink>
+      </li>
+      <li>
+        <NavLink to="/connect">Connect</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="pt-3">
+      <div className="flex justify-between  items-center bg-base-100">
+        <div className=" ">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 ml-0"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="3"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 h-screen text-xl z-[1] mt-3 w-52 p-2 shadow"
+            >
+              {nav}
+            </ul>
+          </div>
+          <a className="btn btn-ghost lg:text-2xl title-text lg:text-right">
+            <div className="ml-28 md:ml-0 lg:ml-0">
+              <span className="text-2xl  lg:text-4xl">LUXURY</span> <br />
+              <span className="text">PROPERTIES</span>
+            </div>
+          </a>
         </div>
-    );
+        <div className=" hidden lg:flex">
+          <ul className="menu menu-horizontal text-xl px-1">{nav}</ul>
+        </div>
+        <div className="w-[70px] h-[70px] rounded-full bg-slate-400 font-bold lg:text-lg btn">
+          <button>LogIn</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
