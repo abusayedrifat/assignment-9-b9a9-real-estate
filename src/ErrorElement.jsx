011f0a9e6d0d1e98a8b367dec27ea429d2d9eab1
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2024-09-27 15:55:46 +0600

    errorElement page updated

--- src/ErrorElement.jsx
+++ src/ErrorElement.jsx
@@ -1,9 +1,15 @@
+import { useNavigate } from "react-router-dom";
 
 const ErrorElement = () => {
+    const navigate =useNavigate()
+    const handleGoBack = ()=>{
+           navigate(-1)
+    }
     return (
-        <div>
-        <h1>404</h1>
-        <h1>Not Found</h1>
+        <div className="flex flex-col justify-center items-center text-center mt-64">
+        <h1 className="text-4xl font-extrabold poppi">404</h1>
+        <h1 className="text-4xl font-extrabold poppi">Not Found</h1>
+        <button onClick={handleGoBack} className="btn bg-[#0b5c52] text-[#C28563]">Go Back</button>
     </div>
     );
 };
